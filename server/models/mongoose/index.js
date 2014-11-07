a975e40772c03d5863ef6f1ace7386e00f0c96d4
Lowry Tang <Lowry@lowry.asiapacific.hpqcorp.net>
2014-11-07 16:10:02 +0800

    delete some files

--- server/models/mongoose/index.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose'),
-  settings = require('../../settings');
-
-var db = mongoose.connect('mongodb://localhost/rss', function (err) {
-  console.log('Connect DB Error : ', err);
-});
-
-module.exports = db;
